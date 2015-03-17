350142c438843e00d6b29325f6a1ea1a482f5bea
Nuwaithu <moehtet@Nu-Wai.local>
2015-03-17 11:46:48 +0630

    Initial Commit

---
+++ netgainsystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netgainsystem
+//
+//  Created by Nu Wai Thu on 3/17/15.
+//  Copyright (c) 2015 NetGain-system. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
